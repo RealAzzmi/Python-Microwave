2e44e9461871f9151da1296092ded3a62ce98abb
Azmi <azmibazeid@gmail.com>
2022-10-25 21:30:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ response_display = widget.ResponseDisplay(window, 45, 1050, 120, "Helvetica 10",
 control.display = display.display
 control.response_display = response_display.response_display
 
-# The 10 buttons represent 0 to 9
+# The 10 buttons represents 0 to 9
 
 button_1 = widget.DigitButton(window, "1", 1080, 200, display.display)
 button_2 = widget.DigitButton(window, "2", 1180, 200,display.display)
@@ -45,11 +45,13 @@ button_0 = widget.DigitButton(window, "0", 1180, 500, display.display)
 enter_button = widget.BooleanButton(window, "Enter", 1250, 490,display.display,response_display.response_display,  width=8, height=2)
 backspacke_button = widget.BooleanButton(window, "⌫", 1050, 490, display.display, response_display.response_display, width=5,height=2)
 
-manual_button = widget.BooleanButton(window, "Manual", 1130, 615,display.display,response_display.response_display ,width=10, height=2)
+manual_button = widget.BooleanButton(window, "Manual", 1130, 585,display.display,response_display.response_display ,width=10, height=2)
 
-automatic_button = widget.BooleanButton(window, "Automatic", 1100, 750,display.display,response_display.response_display, width=14, height=2)
-meat_button = widget.BooleanButton(window, "Meat", 1070, 825,display.display,response_display.response_display,width=8, height=2)
-salad_vegetables = widget.BooleanButton(window, "Vegetables", 1220, 825,display.display,response_display.response_display, width=10, height=2)
+defrost_meat_button = widget.BooleanButton(window, "Defrost\nMeat", 1070, 825,display.display,response_display.response_display,width=8, height=2)
+defrost_vegetables_button = widget.BooleanButton(window, "Defrost\nVegetables", 1220, 825,display.display,response_display.response_display, width=10, height=2)
+
+reheat_button = widget.BooleanButton(window, "Reheat", 1130, 665,display.display,response_display.response_display, width=10, height=2)
+cook_button = widget.BooleanButton(window, "Cook", 1130, 735,display.display,response_display.response_display, width=10, height=2)
 
 # Initial response of the microwave
 control.last_clicked = "Introduction"
