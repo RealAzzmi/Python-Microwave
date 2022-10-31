9ffafd699e3c956703e60c8a6d20cfa7e2ea9545
Azmi <azmibazeid@gmail.com>
2022-10-31 17:46:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import widget
 
 # The window
 window = tk.Tk()
+window.title("Microwave")
 
 # The two frames: Left (Food) and Right (Control)
-food_frame = tk.Frame(window, bg="white", width=1000, height=900)
-control_frame = tk.Frame(window, bg="#000000", width=400, height=900)
+food_frame = tk.Frame(window, bg="white", width=1000, height=700)
+control_frame = tk.Frame(window, bg="#000000", width=400, height=700)
 
 food_frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 control_frame.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True)
@@ -18,8 +19,8 @@ control.window = window
 # Two displays. One is for the input of the user and the timer.
 # The other is for the response of the microwave.
 
-display = widget.Display(window, 13, 1067, 50, "Helvetica 30")
-response_display = widget.ResponseDisplay(window, 45, 1050, 120, "Helvetica 10", "#2f8234")
+display = widget.Display(window, 13, 1050, 50, "Helvetica 30")
+response_display = widget.ResponseDisplay(window, 45, 1030, 120, "Helvetica 10", "#2f8234")
 
 control.display = display.display
 control.response_display = response_display.response_display
@@ -38,20 +39,19 @@ button_7 = widget.DigitButton(window, "7", 1080, 400)
 button_8 = widget.DigitButton(window, "8", 1180, 400)
 button_9 = widget.DigitButton(window, "9", 1280, 400)
 
-button_0 = widget.DigitButton(window, "0", 1180, 500)
+button_0 = widget.DigitButton(window, "0", 1180, 475)
 
 # The rest of the buttons
 
-enter_button = widget.BooleanButton(window,"Enter", 1250, 490, width=8, height=2)
-backspacke_button = widget.BooleanButton(window, "⌫", 1050, 490, width=5,height=2)
+enter_button = widget.BooleanButton(window,"Enter", 1260, 475, width=8, height=2)
+backspacke_button = widget.BooleanButton(window, "⌫", 1070, 475, width=5,height=2)
 
-manual_button = widget.BooleanButton(window, "Manual", 1130, 585, width=10, height=2)
+manual_button = widget.BooleanButton(window, "Manual", 1050, 560, width=10, height=2)
+cook_button = widget.BooleanButton(window, "Cook", 1150, 560, width=10, height=2)
+reheat_button = widget.BooleanButton(window, "Reheat", 1250, 560, width=10, height=2)
 
-defrost_meat_button = widget.BooleanButton(window, "Defrost\nMeat", 1070, 825, width=8, height=2)
-defrost_vegetables_button = widget.BooleanButton(window, "Defrost\nVegetables", 1220, 825, width=10, height=2)
-
-reheat_button = widget.BooleanButton(window, "Reheat", 1130, 665, width=10, height=2)
-cook_button = widget.BooleanButton(window, "Cook", 1130, 735, width=10, height=2)
+defrost_meat_button = widget.BooleanButton(window, "Defrost\nMeat", 1070, 640, width=8, height=2)
+defrost_vegetables_button = widget.BooleanButton(window, "Defrost\nVegetables", 1220, 640, width=10, height=2)
 
 # Initial response of the microwave
 control.last_clicked = "Introduction"
